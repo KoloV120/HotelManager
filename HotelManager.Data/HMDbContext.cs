da24ae8900bdeddb3c6defe4d3a6e8338debe64c
KoloV120 <nikivasilev2976@gmail.com>
2025-04-21 19:23:28 +0300

    Create DbContext + Data Solution

---
+++ HotelManager.Data/HMDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace HotelManager.Data;
+
+public class HMDbContext : DbContext
+{
+    public HMDbContext()
+    {
+
+    }
+    public HMDbContext(DbContextOptions<HMDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Add any additional configuration here if needed
+    }
+}
+
